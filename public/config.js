017485427153478b47e8d7748bc05126e4cf4fb9
LMC <37393495+maotuworkroom@users.noreply.github.com>
2024-05-04 22:08:53 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -7,7 +7,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur1078385-b9802a0868414b8a975d5038',
+    'ur2535041-d549dbf97e318aeefb03f67b',
   ],
 
     // 日志天数
