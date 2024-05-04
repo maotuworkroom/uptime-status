9bdfb55d3a1e95841bb9aa30f6b8b0a600bab574
LMC <37393495+maotuworkroom@users.noreply.github.com>
2024-05-04 22:21:10 +0800

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -23,8 +23,8 @@ function App() {
           ))}
         </div>
         <div id='footer'>
-          <p>基于&nbsp;<Link to='https://uptimerobot.com/' text='UptimeRobot' />&nbsp;接口&nbsp;|&nbsp;检测频率&nbsp;5&nbsp;分钟</p>
-          <p>2020&nbsp;-&nbsp;2023&nbsp;&copy;&nbsp;Reach&nbsp;-&nbsp;<Link to='/' text='墨明' /></p>
+          <p>基于&nbsp;<Link to='https://s.xinu.ink/' text='UptimeRobot' />&nbsp;接口&nbsp;|&nbsp;检测频率&nbsp;5&nbsp;分钟</p>
+          <p>2024&nbsp;&copy;&nbsp;Xinu&nbsp;-&nbsp;<Link to='/' text='MC' /></p>
         </div>
       </div>
     </>
